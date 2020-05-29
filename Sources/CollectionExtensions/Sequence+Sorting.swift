c91aac2e3652834ee7b63c371242607fb4663d59
Sam Deane <sam@elegantchaos.com>
2020-05-29 13:34:21 +0100

    added sorting by keypath

---
+++ Sources/CollectionExtensions/Sequence+Sorting.swift
@@ -0,0 +1,15 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 29/05/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public extension Sequence {
+    /// Returns the sequence, sorted by a keypath.
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
+        return sorted { a, b in
+            return a[keyPath: keyPath] < b[keyPath: keyPath]
+        }
+    }
+}

f687bab61e9730733fb661fd2542779c057799fa
Sam Deane <sam@elegantchaos.com>
2020-10-26 14:14:02 +0000

    added support for inserting a whole sequence into a set

---
+++ Sources/CollectionExtensions/Set+Insertion.swift
@@ -0,0 +1,14 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 26/10/2020.
+//  All code (c) 2020 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+extension Set {
+    mutating func insert<C>(contentsOf collection: C) where C: Collection, C.Element == Element {
+        for item in collection {
+            insert(item)
+        }
+    }
+}
